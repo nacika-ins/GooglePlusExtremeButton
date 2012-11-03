d7665fa434767e51a995e8ef300ff14f55f3bcfe
Takehiro Takahashi <nacika.inscatolare@gmail.com>
2012-11-03 22:38:00 +0900

    Revert "2.78 リリース ミュート機能の誤作動とソースコードの最適化"
    This reverts commit 90ca6c038003acf24cc3075e11a793ded27304a7.

--- debug/option2.js
+++
@@ -1,6 +0,0 @@
-if (lang() == "ja") {
-  document.getElementById("menu").innerHTML = "<div>機能</div><div>ボタン</div><div>挨拶</div><div>初期化</div><div>情報</div><div>連絡</div>";
-}
-else {
-    document.getElementById("menu").innerHTML = "<div>Function</div><div>Button</div><div>Greeting</div><div>Initialization</div><div>Information</div><div>Contact</div>";
-}
